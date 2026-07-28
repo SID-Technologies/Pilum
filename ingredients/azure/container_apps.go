a0ed247bc418d8b89b6aef16f7b337fa7f060aae
Flannel007 <dan.flanagan93@gmail.com>
2026-07-28 00:05:15 -0400

    fixes

--- ingredients/azure/container_apps.go
+++ ingredients/azure/container_apps.go
@@ -2,57 +2,62 @@ package azure
 
 import (
 	"fmt"
+	"strings"
 
 	serviceinfo "github.com/sid-technologies/pilum/lib/service_info"
+	"github.com/sid-technologies/pilum/lib/shellutil"
 )
 
-// GenerateDeployCommand generates an `az containerapp update` command.
-func GenerateDeployCommand(svc serviceinfo.ServiceInfo, imageName string) []string {
+func GenerateDeployCommand(svc serviceinfo.ServiceInfo, imageName string) string {
 	cfg := ParseContainerAppConfig(svc.Config)
+	q := shellutil.Quote
 
-	cmd := []string{
-		"az",
-		"containerapp",
-		"update",
-		"--name", svc.Name,
-		"--resource-group", svc.Project,
-		"--image", imageName,
+	up := []string{
+		"az", "containerapp", "up",
+		"--name", q(svc.Name),
+		"--resource-group", q(svc.Project),
+		"--image", q(imageName),
 	}
-
-	// Add environment if set
 	if cfg.Environment != "" {
-		cmd = append(cmd, "--environment", cfg.Environment)
+		up = append(up, "--environment", q(cfg.Environment))
 	}
-
-	// Add min replicas if set
-	if cfg.MinReplicas >= 0 {
-		cmd = append(cmd, fmt.Sprintf("--min-replicas=%d", cfg.MinReplicas))
-	}
-
-	// Add max replicas if set
-	if cfg.MaxReplicas > 0 {
-		cmd = append(cmd, fmt.Sprintf("--max-replicas=%d", cfg.MaxReplicas))
-	}
-
-	// Add CPU if set
-	if cfg.CPU != "" {
-		cmd = append(cmd, "--cpu", cfg.CPU)
-	}
-
-	// Add memory if set
-	if cfg.Memory != "" {
-		cmd = append(cmd, "--memory", cfg.Memory)
-	}
-
-	// Add ingress settings
 	if cfg.IngressPort > 0 {
-		cmd = append(cmd, "--target-port", fmt.Sprintf("%d", cfg.IngressPort))
+		up = append(up, "--target-port", fmt.Sprintf("%d", cfg.IngressPort))
 		if cfg.IngressExternal {
-			cmd = append(cmd, "--ingress", "external")
+			up = append(up, "--ingress", "external")
 		} else {
-			cmd = append(cmd, "--ingress", "internal")
+			up = append(up, "--ingress", "internal")
 		}
 	}
 
+	// Resource limits are update-only flags; emit the chaser command only
+	// when at least one is configured.
+	update := []string{
+		"az", "containerapp", "update",
+		"--name", q(svc.Name),
+		"--resource-group", q(svc.Project),
+	}
+	hasUpdate := false
+	if cfg.MinReplicas >= 0 {
+		update = append(update, fmt.Sprintf("--min-replicas=%d", cfg.MinReplicas))
+		hasUpdate = true
+	}
+	if cfg.MaxReplicas > 0 {
+		update = append(update, fmt.Sprintf("--max-replicas=%d", cfg.MaxReplicas))
+		hasUpdate = true
+	}
+	if cfg.CPU != "" {
+		update = append(update, "--cpu", q(cfg.CPU))
+		hasUpdate = true
+	}
+	if cfg.Memory != "" {
+		update = append(update, "--memory", q(cfg.Memory))
+		hasUpdate = true
+	}
+
+	cmd := strings.Join(up, " ")
+	if hasUpdate {
+		cmd += " && " + strings.Join(update, " ")
+	}
 	return cmd
 }
